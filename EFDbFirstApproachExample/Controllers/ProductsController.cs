62f2ddfd4ea5ec88df598410ac03964aee76c04e
SOunit <workbookwork7@gmail.com>
2022-10-20 21:03:49 -0700

    add products page

---
+++ EFDbFirstApproachExample/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EFDbFirstApproachExample.Models;
+
+namespace EFDbFirstApproachExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Product> products = db.Products.ToList();
+            return View(products);
+        }
+    }
+}
