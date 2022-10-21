b7e6304af15737926187289b575c4fc87190474c
SOunit <workbookwork7@gmail.com>
2022-10-20 20:49:54 -0700

    add category page

---
+++ EFDbFirstApproachExample/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EFDbFirstApproachExample.Models;
+
+namespace EFDbFirstApproachExample.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+
+            List<Category> categories = db.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
