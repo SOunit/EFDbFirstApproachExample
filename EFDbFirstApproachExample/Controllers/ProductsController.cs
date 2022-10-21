adf8fe94c10038457f8652d89fb613934f706ef9
SOunit <workbookwork7@gmail.com>
2022-10-20 22:56:47 -0700

    add product detail page

--- EFDbFirstApproachExample/Controllers/ProductsController.cs
+++ EFDbFirstApproachExample/Controllers/ProductsController.cs
@@ -8,11 +8,25 @@ namespace EFDbFirstApproachExample.Controllers
     public class ProductsController : Controller
     {
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(string search = "")
         {
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
-            List<Product> products = db.Products.ToList();
+
+            // to keep search keyword in view after submit
+            ViewBag.search = search;
+
+            List<Product> products = db.Products
+                //.Where(temp => temp.CategoryID == 1 && temp.Price >= 50000)
+                .Where(temp => temp.ProductName.Contains(search))
+                .ToList();
             return View(products);
+        }
+
+        public ActionResult Details(long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product p = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
+            return View(p);
         }
     }
 }
