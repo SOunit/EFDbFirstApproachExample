7d9c5ddf5e5ab92206211188c5a6e8794c8d8ff4
SOunit <workbookwork7@gmail.com>
2022-10-20 20:54:02 -0700

    add brand-controller

---
+++ EFDbFirstApproachExample/Controllers/BrandsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EFDbFirstApproachExample.Models;
+
+namespace EFDbFirstApproachExample.Controllers
+{
+    public class BrandsController : Controller
+    {
+        // GET: Brands
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Brand> brands = db.Brands.ToList();
+            return View(brands);
+        }
+    }
+}
